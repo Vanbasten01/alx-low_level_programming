221ee54bd30d65ef2b2003920974ead244bd4471
Vanbasten01 <fouad.yasin01@gmail.com>
2023-03-18 03:15:21 +0100

    Task 4

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: "prints all single digit numbers of base 10 starting from 0"
+ *Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
