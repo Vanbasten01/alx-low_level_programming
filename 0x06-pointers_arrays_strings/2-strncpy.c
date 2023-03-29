dd8aa5ac2af94b2a55303c8a1091cb2859d85f0e
Vanbasten01 <fouad.yasin01@gmail.com>
2023-03-29 16:28:35 +0000

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ **_strncpy - Entry point
+ *@dest: Destination to be copied to
+ *@src: String to be copied
+ *@n:  Number of characters from src to be joined
+ *Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
