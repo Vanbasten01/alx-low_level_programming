45730fd526cfb719c0fa4a45d9f432e137013af3
Vanbasten01 <fouad.yasin01@gmail.com>
2023-03-30 18:20:29 +0000

    Task advanced 2

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_number - Entry point
+ *@n: the integer to be printed
+ *Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = n * -1;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
