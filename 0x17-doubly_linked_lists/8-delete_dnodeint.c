bd224e3406410a3a108c4c815e922f08ba27df79
Vanbasten01 <fouad.yasin01@gmail.com>
2023-06-15 19:53:26 +0100

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,19 +8,18 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *trav = *head, *curr = NULL;
+	dlistint_t *trav = *head, *curr = NULL, *nodefree;
 	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
+		nodefree = *head;
 		*head = trav->next;
 		if (*head)
 			(*head)->prev = NULL;
-		/*trav->next->prev = NULL;*/
-		/*trav->next = NULL;*/
+		free(nodefree);
 		return (1);
 	}
 	while (i != index - 1)
@@ -32,11 +31,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if (trav->next == NULL)
 	{
+		nodefree = trav;
 		trav->prev->next = NULL;
+		free(nodefree);
 		return (1);
 	}
 	curr = trav;
-	trav->next = curr->next->next;
-	trav->next->next->prev = curr;
-	return (1);
+	nodefree = trav->next;
+	if (curr->next->next != NULL)
+	{
+		trav->next = curr->next->next;
+		trav->next->next->prev = curr;
+		free(nodefree);
+		return (1);
+	}
+	return (-1);
 }
