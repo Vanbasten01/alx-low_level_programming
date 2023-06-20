b50ad7e53351230a82b4893be457227de6f29b9d
Vanbasten01 <fouad.yasin01@gmail.com>
2023-06-20 03:29:34 +0100

    Advanced Task 2

--- 0x18-dynamic_libraries/jackpot.c
+++ 0x18-dynamic_libraries/jackpot.c
@@ -1,18 +1,16 @@
 #include <unistd.h>
 int printf(__attribute__((unused))const char *format, ...)
 {
-	static int f = 0;
-
-	if (f == 0)
-	{
+	static int f;
+	if (!f)
 		write(1, "9 8 10 24 75 - 9\n", 17);
-	}
+
 	f = 1;
 	return (0);
 }
 
 int puts(__attribute__((unused))const char *s)
 {
-	write(1, "Congratulations, you won the Jackpot!\n", 38);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	return (0);
 }
