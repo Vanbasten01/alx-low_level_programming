f2026ab9e831857d343d9a86ae8858e3ed9de088
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-10 14:20:59 +0100

    tst

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 #include <math.h>
 
 
-
+int interpolation_search(int *array, size_t size, int value);
 int finder(int *array, int left, int right, int value);
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
