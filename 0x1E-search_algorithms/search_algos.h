1d2bb27a453af7a2b659dae7cb28d6c11a907214
Vanbasten01 <fouad.yasin01@gmail.com>
2023-10-10 01:07:22 +0100

    tst

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+
+#endif
+#include <stdio.h>
+#include <math.h>
+
+
+
+int finder(int *array, int left, int right, int value);
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
